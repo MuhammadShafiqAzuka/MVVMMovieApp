ab946590a000ae3db7ddd1db59881b5690077ecf
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-12 21:16:57 +0800

    Initial commit

---
+++ app/src/main/java/com/azuka/mvvmmovieapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.azuka.mvvmmovieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azuka.mvvmmovieapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/azuka/mvvmmovieapp/ui/moviedetails/MovieDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.azuka.mvvmmovieapp.ui.moviedetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azuka.mvvmmovieapp.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
